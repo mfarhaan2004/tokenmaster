4f1310db23d3862da05c0b3b80222a11bbdd9976
Melrick Ujwal Dsouza <127297745+MelChad@users.noreply.github.com>
2023-09-14 18:14:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Retrieve the queue data from localStorage (if it exists)
-    const storedQueueData = JSON.parse(localStorage.getItem("queueData")) || [];
-    let nextTicketNumber = storedQueueData.length > 0 ? Math.max(...storedQueueData) + 1 : 1;
+    let nextTicketNumber = 1;
 
     const enqueueButton = document.getElementById("enqueue-button");
     const queue = document.querySelector(".queue");
     const waitingList = document.getElementById("waiting-list");
     const currentNumberDisplay = document.getElementById("current-number");
-
-    // Initialize the queue with data from localStorage
-    storedQueueData.forEach((ticketNumber) => {
-        addCustomerToQueue(ticketNumber);
-    });
 
     enqueueButton.addEventListener("click", function (e) {
         e.preventDefault(); // Prevent the form from submitting
@@ -22,11 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         if (name) {
             // Add the new customer to the queue
-            addCustomerToQueue(nextTicketNumber);
-
-            // Store the updated queue data in localStorage
-            storedQueueData.push(nextTicketNumber);
-            localStorage.setItem("queueData", JSON.stringify(storedQueueData));
+            addCustomerToQueue(nextTicketNumber, name);
 
             // Update the current number display
             currentNumberDisplay.textContent = nextTicketNumber;
@@ -35,24 +24,23 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
-    // Add logic for dequeueing and serving next customer
+    // Add logic for dequeueing and serving the next customer
     // ...
 
-    function addCustomerToQueue(ticketNumber) {
+    function addCustomerToQueue(ticketNumber, name) {
         // Create a new queue item
         const queueItem = document.createElement("div");
         queueItem.classList.add("queue-item");
         queueItem.innerHTML = `
             <span class="ticket-number">${ticketNumber}</span>
+            <span class="customer-name">${name}</span>
             <!-- Add other customer details here -->
         `;
         queue.appendChild(queueItem);
 
         // Add to the waiting list
         const waitingListItem = document.createElement("li");
-        waitingListItem.textContent = `Customer ${ticketNumber}`;
+        waitingListItem.textContent = `${name} (${ticketNumber})`;
         waitingList.appendChild(waitingListItem);
     }
 });
-
-
