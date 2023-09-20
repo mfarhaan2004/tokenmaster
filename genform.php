1038750c648cff1ab384a34ad12e81ea212021e2
Melrick Ujwal Dsouza <127297745+MelChad@users.noreply.github.com>
2023-09-20 20:53:50 +0530

    Add files via upload

--- genform.php
+++ genform.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$username = "login";
-$password = "34";
-$database = "test";
+$username = "mel";
+$password = "mel123";
+$database = "token";
 
 // Connection
 $connection = new mysqli($host, $username, $password, $database);
@@ -13,12 +13,12 @@ if ($connection->connect_error) {
 }
 
 // Retrieve data 
-$APL_NO = $_POST['application-number'];
+$Applicant_no = $_POST['application-number'];
 $NAME = $_POST['name'];
-$DEPT = $_POST['department'];
+$DEPT = $_POST['dept'];
 
 // SQL query to insert data into the 'login' table
-$sql = "INSERT INTO login (APL_NO, NAME, DEPT) VALUES ('$APL_NO', '$NAME', '$DEPT')";
+$sql = "INSERT INTO login (Applicant_no,name,dept) VALUES ('$Applicant_no', '$NAME', '$DEPT')";
 
 if ($connection->query($sql) === TRUE) {
     header("Location: gen.html");
